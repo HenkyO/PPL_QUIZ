681b6bc1f8f45197147500ff0bfc359be59bf9db
HenkyO <118961291+HenkyO@users.noreply.github.com>
2025-03-07 11:21:52 +0700

    Update test.py

--- test.py
+++ test.py
@@ -22,6 +22,8 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
+# Tambahkan baris berikut untuk menetapkan lokasi binary Chrome
+chrome_options.binary_location = '/usr/bin/google-chrome-stable'
 
 driver = webdriver.Chrome(options=chrome_options)
 
